93d78d8a9a07d4dec190ccc9223e848bef34158b
mtkowen <mtkowen@hotmail.com>
2020-06-14 22:09:40 +0800

    add 56

---
+++ 56.合并区间.c
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=56 lang=c
+ *
+ * [56] 合并区间
+ *
+ * https://leetcode-cn.com/problems/merge-intervals/description/
+ *
+ * algorithms
+ * Medium (40.65%)
+ * Likes:    458
+ * Dislikes: 0
+ * Total Accepted:    104.6K
+ * Total Submissions: 244.9K
+ * Testcase Example:  '[[1,3],[2,6],[8,10],[15,18]]'
+ *
+ * 给出一个区间的集合，请合并所有重叠的区间。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [[1,3],[2,6],[8,10],[15,18]]
+ * 输出: [[1,6],[8,10],[15,18]]
+ * 解释: 区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: [[1,4],[4,5]]
+ * 输出: [[1,5]]
+ * 解释: 区间 [1,4] 和 [4,5] 可被视为重叠区间。
+ * 
+ */
+
+// @lc code=start
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** merge(int** intervals, int intervalsSize, int* intervalsColSize, int* returnSize, int** returnColumnSizes)
+{
+
+}
+
+
+// @lc code=end
+
