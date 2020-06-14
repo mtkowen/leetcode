652858a6ca5916d243c7fae7e5386a8915489c8d
mtkowen <mtkowen@hotmail.com>
2020-06-14 21:59:54 +0800

    solution

--- 35.搜索插入位置.c
+++ 35.搜索插入位置.c
@@ -44,10 +44,22 @@
  */
 
 // @lc code=start
+int searchInsert(int* nums, int numsSize, int target)
+{
+    if(!nums || !numsSize)
+        return 0;
 
-
-int searchInsert(int* nums, int numsSize, int target){
-
+    int l = 0, r = numsSize - 1;
+    int mid = 0;
+    
+    while (l <= r) {  
+        mid = l + (r - l) / 2;
+        if(nums[mid] == target) return mid;
+        else if(nums[mid] < target) l = mid + 1;
+        else r = mid - 1;
+    }
+    
+    return l;
 }
 
 
